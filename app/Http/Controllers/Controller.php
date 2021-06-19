5d930c39c3e17e8aa8b932e5808c23ad0eddf8f6
myckhel <myckhel1@hotmail.com>
2021-06-19 11:49:31 +0100

    created reverse api endpoint

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,18 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Http\Request;
+use App\Models\Geoname;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    function reverse(Request $request){
+      ['lat' => $lat, 'lng' => $lng] = $request;
+
+      $result = Geoname::reverse($lat, $lng)->first();
+
+      return $result;
+    }
 }
