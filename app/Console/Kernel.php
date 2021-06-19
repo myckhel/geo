62b7b219d047d2d1c809cd536d7c5d352def8e8f
myckhel <myckhel1@hotmail.com>
2021-06-19 14:38:33 +0100

    installed artisan ui

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,20 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use MichaelDrennen\Geonames\Console\Admin1Code;
+use MichaelDrennen\Geonames\Console\Admin2Code;
+use MichaelDrennen\Geonames\Console\AlternateName;
+use MichaelDrennen\Geonames\Console\DownloadGeonames;
+use MichaelDrennen\Geonames\Console\FeatureClass;
+use MichaelDrennen\Geonames\Console\FeatureCode;
+use MichaelDrennen\Geonames\Console\Geoname;
+use MichaelDrennen\Geonames\Console\InsertGeonames;
+use MichaelDrennen\Geonames\Console\Install;
+use MichaelDrennen\Geonames\Console\IsoLanguageCode;
+use MichaelDrennen\Geonames\Console\NoCountry;
+use MichaelDrennen\Geonames\Console\PostalCode;
+use MichaelDrennen\Geonames\Console\Status;
+use MichaelDrennen\Geonames\Console\UpdateGeonames;
 
 class Kernel extends ConsoleKernel
 {
@@ -13,7 +27,20 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        Admin1Code::class,
+        Admin2Code::class,
+        AlternateName::class,
+        DownloadGeonames::class,
+        FeatureClass::class,
+        FeatureCode::class,
+        Geoname::class,
+        InsertGeonames::class,
+        Install::class,
+        IsoLanguageCode::class,
+        NoCountry::class,
+        PostalCode::class,
+        Status::class,
+        UpdateGeonames::class,
     ];
 
     /**
